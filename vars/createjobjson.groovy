8aa2270e44913be24e825f310ff686b0e863eea5
MaheedharTalluri <1210815148@gitam.in>
2020-02-04 18:05:01 +0530

    jenkins test

--- vars/createjobjson.groovy
+++ vars/createjobjson.groovy
@@ -8,8 +8,11 @@ String a=jsonObj.ci.jobs.job.job_name
 String jobname=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.ci.jobs.job.dsl_fileName
 String dslfilename=b.replaceAll("\\[", "").replaceAll("\\]","");
-env.DSL_NAME = dslfilename
-env.JENKINS_NAME = jobname
+
+String c=jsonObj.ci.jobs.job.job_type
+String jobtype=c.replaceAll("\\[", "").replaceAll("\\]","");
+
+
 
 
 sh "rm -rf Text.xml"
@@ -19,18 +22,18 @@ sh "rm -rf Text.xml"
 
 sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
 
-sh "java -jar job-dsl-core-1.76-standalone.jar '${dslfilename}'"
+if ( jobtype == "freestyle" )
+{
+sh "java -jar job-dsl-core-1.76-standalone.jar freestyle.groovy"
+}
+if ( jobtype == "pipeline" )
+{
+sh "java -jar job-dsl-core-1.76-standalone.jar pipeline.groovy"
+}
 
 url='http://18.221.47.136:8080/createItem?name='+jobname
 sh "echo ${url}"
 sh "curl -s -XPOST  '$url' -u admin:119767fb81f22e2f10d8594e4201717e53 --data-binary @Test.xml -H Content-Type:text/xml"
-
-
-
-
-
-
-
 }
 
 
