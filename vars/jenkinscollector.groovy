ffadf0cbd585c946211368fee295e7df679594a0
MaheedharTalluri <1210815148@gitam.in>
2020-02-04 19:47:06 +0530

    jenkins test

--- vars/jenkinscollector.groovy
+++ vars/jenkinscollector.groovy
@@ -1,5 +1,5 @@
 def call(){
 
-sh 'curl -X GET http://18.221.47.136:8080/api/json?pretty=true --user admin:119767fb81f22e2f10d8594e4201717e53'
+sh 'curl -X GET http://18.219.210.127:8080/api/json?pretty=true --user admin:119767fb81f22e2f10d8594e4201717e53'
 
 }
--- vars/lastsuccessfulbuild.groovy
+++ vars/lastsuccessfulbuild.groovy
@@ -1,3 +1,3 @@
 def call(jobname){
-sh "curl -s http://18.221.47.136:8080/job/'${jobname}'/lastSuccessfulBuild/api/json --user admin:119767fb81f22e2f10d8594e4201717e53"
+sh "curl -s http://18.219.210.127:8080/job/'${jobname}'/lastSuccessfulBuild/api/json --user admin:119767fb81f22e2f10d8594e4201717e53"
 }
