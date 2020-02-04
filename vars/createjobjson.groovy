c1283601ecb8990634897529e93f6e7a9cf6c968
MaheedharTalluri <1210815148@gitam.in>
2020-02-04 16:13:56 +0530

    jenkins test

--- vars/createjobjson.groovy
+++ vars/createjobjson.groovy
@@ -19,7 +19,7 @@ sh "rm -rf Text.xml"
 
 sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
 
-sh "java -jar job-dsl-core-1.76-standalone.jar '${dsl}'"
+sh "java -jar job-dsl-core-1.76-standalone.jar '${dslfilename}'"
 
 url='http://18.221.47.136:8080/createItem?name='+jobname
 sh "echo ${url}"
