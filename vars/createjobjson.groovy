7406c01fb678b567840925379f8277339b3e1b8c
MaheedharTalluri <1210815148@gitam.in>
2020-02-04 16:04:07 +0530

    jenkins test

--- vars/createjobjson.groovy
+++ vars/createjobjson.groovy
@@ -2,7 +2,7 @@ def call(jsondata){
 def jsonString = jsondata
 println(jsonString)
 def jsonObj = readJSON text: jsonString
-println(jsonObj.ci)
+println(jsonObj.ci.jobs)
 String a=jsonObj.ci.jobs.job.job_name
 
 String jobname=a.replaceAll("\\[", "").replaceAll("\\]","");
