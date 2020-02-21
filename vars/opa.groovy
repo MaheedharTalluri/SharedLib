125496bd06764874a5c58b9abcec93ca1fbc1ffd
MaheedharTalluri <1210815148@gitam.in>
2020-02-21 13:54:54 +0530

    opa test

---
+++ vars/opa.groovy
@@ -0,0 +1,8 @@
+def call(){
+def user = env.BUILD_USER_ID
+echo $user
+sh "sudo nano input.json"
+sh "sudo echo '{' > input.json"
+sh "sudo echo '/"${user}/"' > input.json"
+sh "sudo echo '}' > input.json"
+}
