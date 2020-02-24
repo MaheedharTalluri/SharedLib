fac6ac766808231da9c34cb3a75c22e6308955a8
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 14:58:28 +0530

    groovy edited

--- vars/policy.groovy
+++ vars/policy.groovy
@@ -3,7 +3,6 @@ def call(){
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 
-//sh """curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "access": "build" } }'"""
 
 
 def build = currentBuild.rawbuild
