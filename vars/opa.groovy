5fcaa8482f8bf8667d7215e2b431eeb5ed2a8b4d
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 11:45:43 +0530

    policy.groovy added

--- vars/opa.groovy
+++ vars/opa.groovy
@@ -1,8 +1,4 @@
 def call(){
 def user = env.BUILD_USER_ID
 echo user
-sh "sudo nano input.json"
-sh "sudo echo '{' > input.json"
-sh 'sudo echo '/"${user}/"' > input.json'
-sh "sudo echo '}' > input.json"
 }
