652dd9cde139dfea3e2c581cde6ec3e40cde9433
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 15:06:42 +0530

    groovy edited

--- vars/policy.groovy
+++ vars/policy.groovy
@@ -2,6 +2,17 @@
 
 
 def call(){
+
+
+def build = currentBuild.rawBuild
+def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+def userid = cause.getUserName()
+
+
+
+
+
+
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 
@@ -12,9 +23,6 @@ sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-
 
 
 
-def build = currentBuild.rawBuild
-def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-def userid = cause.getUserName()
 
 
 
