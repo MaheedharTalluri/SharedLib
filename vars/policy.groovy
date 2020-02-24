b2b6028f15495fb5c8cfeb6c0d801fc564c8e374
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 12:01:12 +0530

    policy.groovy added

--- vars/policy.groovy
+++ vars/policy.groovy
@@ -1,6 +1,6 @@
 def call(){
-sh "curl -X PUT http://18.222.66.194:8181/v1/data/myapi/acl --data-binary @/open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
-sh "curl -X PUT http://18.222.66.194:8181/v1/policies/myapi --data-binary @/open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
+sh "curl -X PUT http://18.222.66.194:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
+sh "curl -X PUT http://18.222.66.194:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 def response = sh(script:'curl -X POST http://18.222.66.194:8181/v1/data/myapi/policy/allow --data-binary "{ "input": { "user": "maheedhar", "access": "build" } }" | jq', returnStdout: true)
 println(response)
 }
