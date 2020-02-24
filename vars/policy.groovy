4cc3158f30807f4da4bb08f9e74f7407162e2dec
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 13:11:21 +0530

    groovy edited

--- vars/policy.groovy
+++ vars/policy.groovy
@@ -1,12 +1,13 @@
+import json.groovy.*
 def call(){
 sh "curl -X PUT http://18.222.66.194:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 sh "curl -X PUT http://18.222.66.194:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 sh """curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'"""
-def response = sh(script:"""curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'""", returnStdout: true)
+response = sh(script:"""curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'""", returnStdout: true)
 println(response)
 @NonCPS
-def jsonObj = readJSON text: response
-println(jsonObj.result)
-
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(response)
+println(object.result)
 
 }
