16f7b669ccc1216375ac9ccbeff53eaf124c95d2
MaheedharTalluri <1210815148@gitam.in>
2020-02-24 13:18:35 +0530

    groovy edited

--- vars/policy.groovy
+++ vars/policy.groovy
@@ -1,13 +1,17 @@
-import json.groovy.*
+
 def call(){
 sh "curl -X PUT http://18.222.66.194:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 sh "curl -X PUT http://18.222.66.194:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 sh """curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'"""
-response = sh(script:"""curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.222.66.194:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "maheedhar", "access": "build" } }'""", returnStdout: true)
 println(response)
-@NonCPS
-def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText(response)
-println(object.result)
+if ( response == "{"result":true}" )
+println("You can build a job")
 
+}
+else{
+println("You do not have access to trigger a build")
+if (!continueBuild) {
+    currentBuild.result = 'ABORTED'
+}
 }
