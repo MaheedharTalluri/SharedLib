1e4535d1bb228be89e365477b55c6b4140a07ebd
MaheedharTalluri <1210815148@gitam.in>
2020-03-06 15:52:25 +0530

    committer based groovy edited

--- vars/committerpolicy.groovy
+++ vars/committerpolicy.groovy
@@ -1,6 +1,6 @@
 def call(committername,branch){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics --data-binary @open-policy-agent/METRICS/COMMITTER-BASED/metrics-data.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @open-policy-agent/METRICS/COMMITTER-BASED/metrics.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/metrics-data.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/metrics.rego"
 String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/foo' --header 'Content-Type: application/json' --data-raw '{"committer":[{"name":"'${committername}'","branch_name":"'${branch}'"}]}'""", returnStdout: true)
 println(response)
 }
